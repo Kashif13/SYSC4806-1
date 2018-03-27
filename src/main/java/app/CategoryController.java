2833320db6189c26496763b9aad4e08fe31efdd4
himanish <himanish.kaushal@gmail.com>
2018-03-26 23:03:41 -0400

    Added val (Lombok) usage and styling for category views.

--- src/main/java/app/CategoryController.java
+++ src/main/java/app/CategoryController.java
@@ -1,6 +1,7 @@
 package app;
 
 import app.Program;
+import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,7 +19,7 @@ public class CategoryController {
 
     @RequestMapping("/pickCategory")
     public String pickCategory(Model model){
-        Category category = new Category();
+        val category = new Category();
         model.addAttribute("category", category);
         model.addAttribute("categories", categoryRepo.findAll());
         return "pickCategory";
@@ -34,14 +35,14 @@ public class CategoryController {
 
     @GetMapping("/addCategory")
     public String categoryForm(Model model){
-        Category category = new Category();
+        val category = new Category();
         model.addAttribute("category", category);
         return "categoryForm";
     }
 
     @PostMapping("/addCategory")
     public String categorySubmit(@ModelAttribute("category") Category category, Model model) {
-        Category c = categoryRepo.save(category);
+        val c = categoryRepo.save(category);
         model.addAttribute("categories", categoryRepo.findAll());
         model.addAttribute("newCategory", c);
         return "listCategories";
