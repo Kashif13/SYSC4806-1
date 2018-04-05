70f2945b77289468ac38cd66cbcbfdd8ee845c04
himanish <himanish.kaushal@gmail.com>
2018-04-05 02:14:59 -0400

    Create and edit forms for learning outcome along with course and category selects.

--- src/main/java/app/LearningOutcomeController.java
+++ src/main/java/app/LearningOutcomeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import app.LearningOutcomeRepository;
 
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -19,6 +20,12 @@ public class LearningOutcomeController {
 
     @Autowired
     private ProgramRepository progRepo;
+
+    @Autowired
+    private CourseRepository courseRepo;
+
+    @Autowired
+    private CategoryRepository categoryRepo;
 
 
     @GetMapping("/learningOutcome")
@@ -49,29 +56,64 @@ public class LearningOutcomeController {
 
     @GetMapping("/addLearningOutcome")
     public String learningOutcomeForm(Model model){
-        LearningOutcome learningOutcome = new LearningOutcome();
-        model.addAttribute("learningOutcome", learningOutcome);
+
+        LearningOutcomeForm learningOutcomeForm = new LearningOutcomeForm();
+
+        List<Course> courses = courseRepo.findAll();
+        List<Category> categories = categoryRepo.findAll();
+
+        model.addAttribute("learningOutcomeForm", learningOutcomeForm);
+        model.addAttribute("courses", courses);
+        model.addAttribute("categories", categories);
+
         return "learningOutcomeForm";
     }
 
     @PostMapping("/addLearningOutcome")
-    public String learningOutcomeSubmit(@ModelAttribute("learningOutcome") LearningOutcome learningOutcome, Model model) {
-        LearningOutcome lo = learningOutcomeRepo.save(learningOutcome);
+    public String learningOutcomeSubmit(@ModelAttribute("learningOutcomeForm") LearningOutcomeForm learningOutcomeForm, Model model) {
+
+        LearningOutcome lo = learningOutcomeForm.getLearningOutcome();
+        lo.setCourse(learningOutcomeForm.getCourse());
+        lo.setCategory(learningOutcomeForm.getCategory());
+        LearningOutcome newLearningOutcome = learningOutcomeRepo.save(lo);
+
         model.addAttribute("learningOutcomes", learningOutcomeRepo.findAll());
-        model.addAttribute("newLearningOutcome", lo);
+        model.addAttribute("newLearningOutcome", newLearningOutcome);
+
         return "listLearningOutcomes";
     }
 
     @GetMapping("/editLearningOutcome/{id}")
     public String editLearningOutcome(@PathVariable Long id, Model model){
-        LearningOutcome learningOutcome = learningOutcomeRepo.findById(id);
-        model.addAttribute("learningOutcome", learningOutcome);
+
+        LearningOutcomeForm learningOutcomeForm = new LearningOutcomeForm();
+        LearningOutcome lo = learningOutcomeRepo.findOne(id);
+        Long courseId = lo.getCourse().getId();
+        Long categoryId = lo.getCategory().getId();
+
+        learningOutcomeForm.setLearningOutcome(lo);
+        learningOutcomeForm.setCourse(lo.getCourse());
+        learningOutcomeForm.setCategory(lo.getCategory());
+
+        List<Course> courses = courseRepo.findAll();
+        List<Category> categories = categoryRepo.findAll();
+
+        model.addAttribute("learningOutcomeForm", learningOutcomeForm);
+        model.addAttribute("courses", courses);
+        model.addAttribute("categories", categories);
+        model.addAttribute("courseId", courseId);
+        model.addAttribute("categoryId", categoryId);
+
+
         return "editLearningOutcomeForm";
     }
 
     @PostMapping("/updateLearningOutcome/{id}")
-    public String updateLearningOutcome(@PathVariable Long id, @ModelAttribute("learningOutcome") LearningOutcome learningOutcome, Model model) {
+    public String updateLearningOutcome(@PathVariable Long id, @ModelAttribute("learningOutcomeForm") LearningOutcomeForm learningOutcomeForm, Model model) {
+        LearningOutcome learningOutcome = learningOutcomeForm.getLearningOutcome();
         learningOutcome.setId(id);
+        learningOutcome.setCourse(learningOutcomeForm.getCourse());
+        learningOutcome.setCategory(learningOutcomeForm.getCategory());
         LearningOutcome updatedLearningOutcome = learningOutcomeRepo.save(learningOutcome);
         model.addAttribute("learningOutcomes", learningOutcomeRepo.findAll());
         model.addAttribute("updatedLearningOutcome", updatedLearningOutcome);
